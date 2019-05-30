518a504852faf02b9590aa42c2233e9d5440879d
Kurt Leadley <kleadley@my365.bellevue.edu>
2019-05-30 02:14:14 -0400

    Exercise 1.3 Hello World

---
+++ week-1/hello-world.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title: Assignment 1.3
+; Author: Professor Krasso
+; Date:  5/30/2019
+; Modified By: Kurt Leadley
+; Description: This program was used to learn the Node.js debugger
+;===========================================
+*/ 
+console.log('Hello World, my name is Kurt Leadley');

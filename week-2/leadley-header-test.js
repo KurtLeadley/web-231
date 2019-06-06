f1d1e620d5286e50e00ab5393f10f57fdc3478a6
Kurt Leadley <kleadley@my365.bellevue.edu>
2019-06-05 23:02:38 -0400

    Adding header includes file

---
+++ week-2/leadley-header-test.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Professor Krasso
+; Date:   05 June 2019
+; Modified by: Kurt Leadley
+; Description: Displays a formatted header
+; with console.log
+;===========================================
+*/
+
+const header = require('../leadley-header.js');
+// make sure to console.log this function call to see if it actually works
+var outPutHeader = header.display("Kurt","Leadley","leadley-2.2");
+console.log(outPutHeader);
+

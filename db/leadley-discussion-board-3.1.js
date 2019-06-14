40828cbce6e2b948d144c29706d01764269c0b6d
Kurt Leadley <kleadley@my365.bellevue.edu>
2019-06-14 01:36:32 -0400

    DB week 3 submission

---
+++ db/leadley-discussion-board-3.1.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title: Assignment 3.1
+; Author: Kurt Leadley
+; Date: 6/13/2019
+; Modified By: No one yet.
+; Description: Just a nice and easy loop through an array. Take the original array, add teams that start with "S"
+; to the "sTeamsOnlyArray", add all other teams to the "allOtherTeamsArray"
+;===========================================
+*/
+
+var sportsTeamArray = ["Sharks","Raptors","Steelers","Chargers","Red Sox","Sun Devils"];
+var sTeamsOnlyArray = [];
+var allOtherTeamsArray = [];
+for ($i = 0; $i < count(sportsTeamArray); $i++) {
+  var sportsTeam = sportsTeamArray[$i];
+  if (sportsTeam.startsWith("S")) {
+    sTeamsOnlyArray.push(sportsTeam);
+  } else {
+    sportsTeam.push(allOtherTeamsArray);
+  }
+}
+console.log("S teams array: " + sTeamsOnlyArray);
+console.log("All other teams array: " +allOtherTeamsArray);
